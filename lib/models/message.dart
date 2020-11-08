a31611d38cf69132f470812bc98be00e1e5150a5
Makhmudov Babur <bobs4462@gmail.com>
2020-11-08 20:04:37 +0300

    :star2: Added models for message

---
+++ lib/models/message.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/foundation.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'message.g.dart';
+
+// TODO document the model
+
+@JsonSerializable(explicitToJson: true)
+class Message {
+  @JsonKey(required: true)
+  String id;
+
+  @JsonKey(name: 'responses_count')
+  int responsesCount;
+
+  @JsonKey(required: true)
+  String sender;
+
+  @JsonKey(required: true, name: 'creation_date')
+  int creationDate;
+
+  @JsonKey(required: true)
+  MessageTwacode content;
+
+  Map<String, dynamic> reactions;
+
+  List<Message> responses;
+
+  Message({
+    @required this.id,
+    this.responsesCount,
+    @required this.sender,
+    @required this.creationDate,
+    @required this.content,
+    this.reactions,
+    this.responses,
+  });
+
+  /// Convenience methods to avoid serializing this class from JSON
+  /// https://flutter.dev/docs/development/data-and-backend/json#code-generation
+  factory Message.fromJson(Map<String, dynamic> json) =>
+      _$MessageFromJson(json);
+
+  /// Convenience methods to avoid serializing this class to JSON
+  /// https://flutter.dev/docs/development/data-and-backend/json#code-generation
+  Map<String, dynamic> toJson() => _$MessageToJson(this);
+}
+
+@JsonSerializable()
+class MessageTwacode {
+  @JsonKey(required: true, name: 'original_str')
+  final String originalStr;
+
+  @JsonKey(required: true)
+  final List<dynamic> prepared;
+
+  MessageTwacode({
+    @required this.originalStr,
+    @required this.prepared,
+  });
+
+  /// Convenience methods to avoid serializing this class from JSON
+  /// https://flutter.dev/docs/development/data-and-backend/json#code-generation
+  factory MessageTwacode.fromJson(Map<String, dynamic> json) =>
+      _$MessageTwacodeFromJson(json);
+
+  /// Convenience methods to avoid serializing this class to JSON
+  /// https://flutter.dev/docs/development/data-and-backend/json#code-generation
+  Map<String, dynamic> toJson() => _$MessageTwacodeToJson(this);
+}
---
+++ lib/models/message.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'message.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Message _$MessageFromJson(Map<String, dynamic> json) {
+  $checkKeys(json,
+      requiredKeys: const ['id', 'sender', 'creation_date', 'content']);
+  return Message(
+    id: json['id'] as String,
+    responsesCount: json['responses_count'] as int,
+    sender: json['sender'] as String,
+    creationDate: json['creation_date'] as int,
+    content: json['content'] == null
+        ? null
+        : MessageTwacode.fromJson(json['content'] as Map<String, dynamic>),
+    reactions: json['reactions'] as Map<String, dynamic>,
+    responses: (json['responses'] as List)
+        ?.map((e) =>
+            e == null ? null : Message.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
+}
+
+Map<String, dynamic> _$MessageToJson(Message instance) => <String, dynamic>{
+      'id': instance.id,
+      'responses_count': instance.responsesCount,
+      'sender': instance.sender,
+      'creation_date': instance.creationDate,
+      'content': instance.content?.toJson(),
+      'reactions': instance.reactions,
+      'responses': instance.responses?.map((e) => e?.toJson())?.toList(),
+    };
+
+MessageTwacode _$MessageTwacodeFromJson(Map<String, dynamic> json) {
+  $checkKeys(json, requiredKeys: const ['original_str', 'prepared']);
+  return MessageTwacode(
+    originalStr: json['original_str'] as String,
+    prepared: json['prepared'] as List,
+  );
+}
+
+Map<String, dynamic> _$MessageTwacodeToJson(MessageTwacode instance) =>
+    <String, dynamic>{
+      'original_str': instance.originalStr,
+      'prepared': instance.prepared,
+    };
