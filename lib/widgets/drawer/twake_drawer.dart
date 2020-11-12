a04fdad2f72597853bc4558d2a33d58f7d2935b1
Makhmudov Babur <bobs4462@gmail.com>
2020-11-12 18:01:23 +0300

    :rainbow: Pretty Drawer for twake

--- lib/widgets/drawer/twake_drawer.dart
+++ lib/widgets/drawer/twake_drawer.dart
@@ -13,6 +13,16 @@ class TwakeDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     final profile = Provider.of<ProfileProvider>(context);
     final workspaces = profile.companyWorkspaces(profile.companies[0].id);
+    final user = profile.currentProfile;
+    final padding = EdgeInsets.symmetric(
+      horizontal: DimensionsConfig.widthMultiplier * 3,
+      vertical: DimensionsConfig.heightMultiplier,
+    );
+    final shadow = BoxShadow(
+      offset: Offset(-10, 10),
+      blurRadius: 10,
+      color: Color.fromRGBO(0, 21, 53, 1),
+    );
     return Container(
       width: DimensionsConfig.widthMultiplier * 80,
       child: Drawer(
@@ -20,18 +30,15 @@ class TwakeDrawer extends StatelessWidget {
           decoration: BoxDecoration(
             color: Color.fromRGBO(15, 29, 54, 1),
           ), // TODO decorate the container
-          padding: EdgeInsets.symmetric(
-            horizontal: DimensionsConfig.widthMultiplier * 3,
-            vertical: DimensionsConfig.heightMultiplier,
-          ),
+          // padding: EdgeInsets.symmetric(
+          // horizontal: DimensionsConfig.widthMultiplier * 3,
+          // vertical: DimensionsConfig.heightMultiplier,
+          // ),
           child: Column(
             children: [
               Container(
-                decoration: BoxDecoration(boxShadow: [
-                  BoxShadow(
-                    color: Color.fromRGBO(12, 28, 54, 1),
-                  ),
-                ]),
+                padding: padding,
+                decoration: BoxDecoration(boxShadow: [shadow]),
                 child: Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
@@ -48,10 +55,10 @@ class TwakeDrawer extends StatelessWidget {
                           alignment: Alignment.centerRight,
                           child: IconButton(
                             onPressed: () {},
+                            iconSize: DimensionsConfig.textMultiplier *
+                                ICON_SIZE_MULTIPLIER,
                             icon: Icon(
                               Icons.settings_outlined,
-                              size: DimensionsConfig.textMultiplier *
-                                  ICON_SIZE_MULTIPLIER,
                               color: Colors.white,
                             ),
                           ),
@@ -60,32 +67,36 @@ class TwakeDrawer extends StatelessWidget {
                     ]),
               ),
               SizedBox(height: DimensionsConfig.heightMultiplier * 3),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Workspaces',
-                    style: Theme.of(context).textTheme.headline5,
-                  ), // TODO configure the styles
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.add_sharp,
-                          size: DimensionsConfig.textMultiplier *
+              Padding(
+                padding: padding,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Workspaces',
+                      style: Theme.of(context).textTheme.headline5,
+                    ), // TODO configure the styles
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: IconButton(
+                          onPressed: () {},
+                          iconSize: DimensionsConfig.textMultiplier *
                               ICON_SIZE_MULTIPLIER,
-                          color: Colors.white,
+                          icon: Icon(
+                            Icons.add_sharp,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
               SizedBox(height: DimensionsConfig.heightMultiplier * 2),
               Container(
-                height: 70 * DimensionsConfig.heightMultiplier,
+                height: 50 * DimensionsConfig.heightMultiplier,
+                padding: padding,
                 child: ListView.separated(
                     separatorBuilder: (ctx, i) => Divider(),
                     itemCount: workspaces.length,
@@ -101,6 +112,37 @@ class TwakeDrawer extends StatelessWidget {
                             ),
                           ],
                         )),
+              ),
+              Spacer(),
+              Container(
+                padding: padding,
+                decoration: BoxDecoration(boxShadow: [shadow]),
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ImageAvatar(user.thumbnail),
+                      SizedBox(
+                        width: DimensionsConfig.widthMultiplier * 2,
+                      ),
+                      Text(
+                        '${user.firstName} ${user.lastName}',
+                        style: Theme.of(context).textTheme.headline5,
+                      ), // TODO configure the styles
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.login_outlined,
+                              size: DimensionsConfig.textMultiplier *
+                                  ICON_SIZE_MULTIPLIER,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
               ),
             ],
           ),
