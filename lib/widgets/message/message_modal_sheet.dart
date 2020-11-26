4b70ad03554206e573be801416d954fa10aeccd9
Makhmudov Babur <bobs4462@gmail.com>
2020-11-26 23:19:02 +0300

    :star2: Added emoji line to bottom modal sheet

--- lib/widgets/message/message_modal_sheet.dart
+++ lib/widgets/message/message_modal_sheet.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:twake_mobile/config/dimensions_config.dart' show Dim;
 import 'package:twake_mobile/models/message.dart';
 import 'package:twake_mobile/providers/profile_provider.dart';
+import 'package:twake_mobile/utils/emojis.dart';
 
 class MessageModalSheet extends StatelessWidget {
   final Message message;
@@ -25,6 +26,8 @@ class MessageModalSheet extends StatelessWidget {
         children: [
           /// Show edit only if the sender of the message is the person,
           /// who's currently logged in
+          EmojiLine(),
+          Divider(),
           if (isMe)
             ListTile(
               leading: Icon(Icons.edit_outlined),
@@ -62,5 +65,35 @@ class MessageModalSheet extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class EmojiLine extends StatelessWidget {
+  static const EMOJISET = [
+    'smiley',
+    'sweat_smile',
+    'thumbsup',
+    'thumbsdown',
+    'laughing',
+    'heart',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        // mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ...EMOJISET.map((e) => InkWell(
+                child: Text(
+                  Emojis.getByName(e),
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+              )),
+          IconButton(
+            icon: Icon(Icons.tag_faces),
+            onPressed: () {},
+            iconSize: Dim.tm4(),
+          ),
+        ]);
   }
 }
