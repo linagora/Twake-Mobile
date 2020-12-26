624112526f36867bb3fe7a4f3b6f6341d953a0c4
Makhmudov Babur <bobs4462@gmail.com>
2020-12-26 22:38:50 +0300

    Added batch users load

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,12 +1,9 @@
 import 'dart:collection';
-
 import 'package:twake/models/user.dart';
 import 'package:twake/services/service_bundle.dart';
 
-const _BUFFER_SIZE = 250;
-
 class UserRepository {
-  ListQueue<User> items = ListQueue(_BUFFER_SIZE);
+  Map<String, User> items = {};
   static UserRepository _userRepository;
   final String apiEndpoint;
 
@@ -24,12 +21,12 @@ class UserRepository {
   final logger = Logger();
 
   Future<User> user(String userId) async {
-    User item = items.firstWhere((i) => i.id == userId, orElse: () => null);
+    User item = items[userId];
     if (item == null) {
       final userMap = await _storage.load(type: StorageType.User, key: userId);
       if (userMap != null) {
         item = User.fromJson(userMap);
-        items.add(item);
+        items[userId] = item;
       }
     }
     if (item == null) {
@@ -37,20 +34,39 @@ class UserRepository {
       final Map userMap = list[0];
       if (userMap.isNotEmpty) {
         item = User.fromJson(userMap);
-        items.add(item);
+        items[userId] = item;
         save(item);
       }
     }
     return item;
   }
 
-  Future<void> batchUsersLoad(Set<String> userIds) async {
-    final List response = await _api.get(apiEndpoint, params: {
-      'id': userIds,
-    });
+  Future<void> batchUsersLoad(HashSet<String> userIds) async {
+    items.removeWhere((id, _) => !userIds.contains(id));
+    userIds.retainAll(items.keys);
+    List<String> toBeFetched = [];
+    for (String id in userIds) {
+      final item = await _storage.load(type: StorageType.User, key: id);
+      if (item == null)
+        toBeFetched.add(id);
+      else
+        items[id] = User.fromJson(item);
+    }
+    final List response = await _api.get(
+      apiEndpoint,
+      params: {
+        'id': toBeFetched,
+      },
+    );
     items.clear();
-    final users = response.map((u) => User.fromJson(u));
-    items.addAll(users);
+    final users =
+        response.map((u) => User.fromJson(u)).map((u) => MapEntry(u.id, u));
+    items.addEntries(users);
+    users.forEach((u) => _storage.store(
+          item: u.value,
+          type: StorageType.User,
+          key: u.value.id,
+        ));
     logger.d('Loaded ${items.length} users for messages');
   }
 
