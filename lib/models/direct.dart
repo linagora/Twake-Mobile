4499793984718bedd099426c1473bc9c90cfff87
Makhmudov Babur <bobs4462@gmail.com>
2020-12-28 12:47:45 +0300

    Cleaned up directs model

--- lib/models/direct.dart
+++ lib/models/direct.dart
@@ -1,7 +1,7 @@
+import 'dart:convert' show jsonEncode, jsonDecode;
+
 import 'package:json_annotation/json_annotation.dart';
-// import 'package:twake/config/dimensions_config.dart' show Dim;
 import 'package:twake/models/collection_item.dart';
-// import 'package:twake/widgets/common/image_avatar.dart';
 
 part 'direct.g.dart';
 
@@ -36,39 +36,6 @@ class Direct extends CollectionItem {
 
   @JsonKey(required: true, name: 'messages_unread')
   int messageUnread;
-
-  // List<DirectMember> getCorrespondents(profile) {
-  // final correspondents = members.where((m) {
-  // return !profile.isMe(m.userId);
-  // }).toList();
-  // return correspondents;
-  // }
-
-  // List<Widget> buildCorrespondentAvatars(profile) {
-  // final correspondents = getCorrespondents(profile);
-  // List<Padding> paddedAvatars = [];
-  // for (int i = 0; i < correspondents.length; i++) {
-  // paddedAvatars.add(Padding(
-  // padding: EdgeInsets.only(left: i * Dim.wm2),
-  // child: ImageAvatar(correspondents[i].thumbnail)));
-  // }
-  // return paddedAvatars;
-  // }
-
-  // String buildDirectName(profile) {
-  // if (this.name.isNotEmpty) return this.name;
-//
-  // final correspondents = getCorrespondents(profile);
-  // if (correspondents.length == 1) {
-  // return '${correspondents[0].firstName} ${correspondents[0].lastName}';
-  // }
-  // String name =
-  // '${correspondents[0].firstName} ${correspondents[0].lastName}';
-  // for (int i = 1; i < correspondents.length; i++) {
-  // name += ', ${correspondents[i].firstName} ${correspondents[i].lastName}';
-  // }
-  // return name;
-  // }
 
   Direct({
     this.id,
