fffdbc19d287af56cb7220e4600fdfccf52a74ab
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-01-14 18:57:23 +0300

    ChannelTypeForm layout update.

--- lib/widgets/sheets/channel_type_form.dart
+++ lib/widgets/sheets/channel_type_form.dart
@@ -16,18 +16,38 @@ class _ChannelTypeFormState extends State<ChannelTypeForm> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         SheetTitleBar(
           title: 'New Channel',
+          leadingTitle: 'Back',
+          leadingAction: () => print('BACK!'),
           trailingTitle: 'Create',
           trailingAction: () => print('CREATE!'),
         ),
-        SizedBox(height: 16),
+        SizedBox(height: 23),
+        Padding(
+          padding: const EdgeInsets.only(left: 14.0, right: 100.0),
+          child: Text(
+            'CHANNEL TYPE',
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              fontSize: 13.0,
+              fontWeight: FontWeight.w400,
+              color: Colors.black.withOpacity(0.4),
+            ),
+          ),
+        ),
+        SizedBox(height: 6),
+        ChannelTypesContainer(
+          selectedType: ChannelType.public,
+        ),
+        SizedBox(height: 8),
         HintLine(
           text:
               'Direct channels involve correspondence between selected members',
         ),
-        SizedBox(height: 20),
         // Container(
         //   padding: const EdgeInsets.only(left: 14.0, right: 7),
         //   color: Colors.white,
@@ -53,7 +73,31 @@ class ChannelTypesContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Divider(
+          thickness: 0.5,
+          height: 0.5,
+          color: Colors.black.withOpacity(0.2),
+        ),
+        SelectableItem(
+          title: 'Public',
+          selected: selectedType == ChannelType.public,
+          onTap: () => print('select public'),
+        ),
+        SelectableItem(
+          title: 'Private',
+          selected: selectedType == ChannelType.private,
+          onTap: () => print('select private'),
+        ),
+        SelectableItem(
+          title: 'Direct',
+          selected: selectedType == ChannelType.direct,
+          onTap: () => print('select direct'),
+        ),
+      ],
+    );
   }
 }
 
@@ -71,25 +115,43 @@ class SelectableItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            'Public',
-            style: TextStyle(
-              fontSize: 17.0,
-              fontWeight: FontWeight.w400,
-              color: Colors.black,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 44.0,
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 14.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                    ),
+                  ),
+                  if (selected)
+                    Icon(
+                      CupertinoIcons.check_mark,
+                      color: Color(0xff837cfe),
+                    ),
+                ],
+              ),
             ),
-          ),
-          if (selected)
-            Icon(
-              CupertinoIcons.check_mark,
-              color: Color(0xff837cfe),
+            Divider(
+              indent: 15.0,
+              thickness: 0.5,
+              height: 0.5,
+              color: Colors.black.withOpacity(0.2),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
