d9a91aafe5e8e52f2a7c5f662bc431a287de1f41
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-01-15 17:29:10 +0300

    Menu image replaced.

--- lib/widgets/bars/main_app_bar.dart
+++ lib/widgets/bars/main_app_bar.dart
@@ -13,37 +13,30 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       titleSpacing: 0.0,
-      leading: IconButton(
-        padding: EdgeInsets.only(left: Dim.wm3),
-        onPressed: () {
-          scaffoldKey.currentState.openDrawer();
-        },
-        icon: Icon(
-          Icons.menu,
-          size: Dim.tm4(),
-        ),
+      leading: InkWell(
+        onTap: () => scaffoldKey.currentState.openDrawer(),
+        child: Image.asset('assets/images/menu.png'),
       ),
       backgroundColor: Colors.white,
       toolbarHeight: Dim.heightPercent(
         (kToolbarHeight * 0.15).round(),
       ),
       // taking into account current appBar height to calculate a new one
-      title: BlocBuilder<WorkspacesBloc, WorkspaceState>(
-          builder: (ctx, state) {
-            if (state is WorkspacesLoaded)
-              return ListTile(
-                contentPadding: EdgeInsets.zero,
-                leading: ImageAvatar(state.selected.logo),
-                title: Text(
-                  state.selected.name,
-                  style: Theme.of(context).textTheme.headline6,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                ),
-              );
-            else
-              return CircularProgressIndicator();
-          }),
+      title: BlocBuilder<WorkspacesBloc, WorkspaceState>(builder: (ctx, state) {
+        if (state is WorkspacesLoaded)
+          return ListTile(
+            contentPadding: EdgeInsets.zero,
+            leading: ImageAvatar(state.selected.logo),
+            title: Text(
+              state.selected.name,
+              style: Theme.of(context).textTheme.headline6,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+          );
+        else
+          return CircularProgressIndicator();
+      }),
     );
   }
 
