2aef3a51f295c03474f14d2e07318e43f6639959
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-01-18 17:25:40 +0300

    Drafts util update.

--- lib/utils/drafts.dart
+++ lib/utils/drafts.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
 import 'package:twake/services/service_bundle.dart';
 
@@ -11,35 +12,35 @@ enum DraftType {
 
 class Drafts {
   static var _draftsMap = {};
-  static var _reverseDraftsMap = {};
-
   static final _storage = Storage();
 
   static Future<void> load() async {
-    var map = await _storage.load(
+    final map = await _storage.load(
       type: StorageType.Drafts,
       key: _DRAFTS_STORE_KEY,
       fields: _storage.settingsField != null ? [_storage.settingsField] : null,
     );
+    _draftsMap = jsonDecode(map[_storage.settingsField]);
+  }
 
-    // if (map == null) {
-    //   map = await _api.get(Endpoint.emojis);
-    //   await _storage.store(
-    //     item: {
-    //       'id': _EMOJIS_STORE_KEY,
-    //       _storage.settingsField: jsonEncode(map)
-    //     },
-    //     type: StorageType.Emojis,
-    //   );
-    //   _emojimap = map;
-    // } else {
-    _draftsMap= jsonDecode(map[_storage.settingsField]);
-    // }
-    _reverseDraftsMap = _draftsMap.map((k, v) => MapEntry(v, k));
+  static Future<void> save({
+    @required String id,
+    @required DraftType type,
+    @required String draft,
+  }) async {
+    final key = '$id-${describeEnum(type)}';
+    _draftsMap[key] = draft;
+    await _storage.store(
+      item: {
+          'id': _DRAFTS_STORE_KEY,
+          _storage.settingsField: jsonEncode(_draftsMap),
+        },
+      type: StorageType.Drafts,
+    );
   }
 
   static String getById(String id, {@required DraftType forType}) {
-    var key = '$id-$forType';
+    var key = '$id-${describeEnum(forType)}';
     return _draftsMap[key] ?? '';
   }
 }
