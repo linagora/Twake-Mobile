d8013fb573b3d672085593c549c8f3ae74b88f40
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-01-25 12:03:56 +0300

    Working on sliding panel replacement.

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:twake/blocs/channels_bloc.dart';
 import 'package:twake/blocs/directs_bloc.dart';
 import 'package:twake/blocs/sheet_bloc.dart';
@@ -63,121 +64,101 @@ class _MainPageState extends State<MainPage>
       key: _scaffoldKey,
       drawer: TwakeDrawer(),
       backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          // MainPage body
-          SafeArea(
-            child: Column(
-              children: [
-                MainAppBar(
-                  scaffoldKey: _scaffoldKey,
-                ),
-                Expanded(
-                  child: BlocListener<ChannelsBloc, ChannelState>(
-                    listener: (ctx, state) {
-                      if (state is ErrorLoadingChannels)
-                        Scaffold.of(ctx).showSnackBar(SnackBar(
-                          content: Text('No connection to internet'),
-                          backgroundColor: Theme.of(ctx).errorColor,
-                        ));
-                    },
-                    child: BlocBuilder<ChannelsBloc, ChannelState>(
-                      builder: (ctx, state) => (state is ChannelsLoaded ||
-                              state is ChannelsEmpty)
-                          ? RefreshIndicator(
-                              onRefresh: () {
-                                BlocProvider.of<ChannelsBloc>(ctx)
-                                    .add(ReloadChannels(forceFromApi: true));
-                                BlocProvider.of<DirectsBloc>(ctx)
-                                    .add(ReloadChannels(forceFromApi: true));
-                                return Future.delayed(Duration(seconds: 1));
-                              },
-                              child: GestureDetector(
-                                onTap: () => _closeSheet(),
-                                behavior: HitTestBehavior.translucent,
-                                child: Padding(
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 12.0,
-                                  ),
-                                  child: ListView(
-                                    padding: EdgeInsets.only(top: 0),
-                                    children: [
-                                      // Starred channels will be implemented in version 2
-                                      // StarredChannelsBlock([]),
-                                      // Divider(height: Dim.hm5),
-                                      ChannelsGroup(),
-                                      Divider(
-                                        thickness: 2.0,
-                                        height: 2.0,
-                                        color: Color(0xffEEEEEE),
-                                      ),
-                                      SizedBox(height: 8),
-                                      DirectMessagesGroup(),
-                                      Divider(
-                                        thickness: 2.0,
-                                        height: 2.0,
-                                        color: Color(0xffEEEEEE),
-                                      ),
-                                      SizedBox(height: Dim.hm2),
-                                    ],
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: SlidingUpPanel(
+          minHeight: 0,
+          panel: BlocBuilder<SheetBloc, SheetState>(
+              buildWhen: (_, current) =>
+                  current is SheetShouldOpen || current is SheetShouldClose,
+              builder: (context, state) {
+                return DraggableScrollable();
+              }),
+          body: Column(
+            children: [
+              MainAppBar(
+                scaffoldKey: _scaffoldKey,
+              ),
+              Expanded(
+                child: BlocListener<ChannelsBloc, ChannelState>(
+                  listener: (ctx, state) {
+                    if (state is ErrorLoadingChannels)
+                      Scaffold.of(ctx).showSnackBar(SnackBar(
+                        content: Text('No connection to internet'),
+                        backgroundColor: Theme.of(ctx).errorColor,
+                      ));
+                  },
+                  child: BlocBuilder<ChannelsBloc, ChannelState>(
+                    builder: (ctx, state) =>
+                        (state is ChannelsLoaded || state is ChannelsEmpty)
+                            ? RefreshIndicator(
+                                onRefresh: () {
+                                  BlocProvider.of<ChannelsBloc>(ctx)
+                                      .add(ReloadChannels(forceFromApi: true));
+                                  BlocProvider.of<DirectsBloc>(ctx)
+                                      .add(ReloadChannels(forceFromApi: true));
+                                  return Future.delayed(Duration(seconds: 1));
+                                },
+                                child: GestureDetector(
+                                  onTap: () => _closeSheet(),
+                                  behavior: HitTestBehavior.translucent,
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 12.0,
+                                    ),
+                                    child: ListView(
+                                      padding: EdgeInsets.only(top: 0),
+                                      children: [
+                                        // Starred channels will be implemented in version 2
+                                        // StarredChannelsBlock([]),
+                                        // Divider(height: Dim.hm5),
+                                        ChannelsGroup(),
+                                        Divider(
+                                          thickness: 2.0,
+                                          height: 2.0,
+                                          color: Color(0xffEEEEEE),
+                                        ),
+                                        SizedBox(height: 8),
+                                        DirectMessagesGroup(),
+                                        Divider(
+                                          thickness: 2.0,
+                                          height: 2.0,
+                                          color: Color(0xffEEEEEE),
+                                        ),
+                                        SizedBox(height: Dim.hm2),
+                                      ],
+                                    ),
                                   ),
                                 ),
-                              ),
-                            )
-                          : Center(child: CircularProgressIndicator()),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          // Blur under the sheet
-          Container(
-            width: _shouldBlur ? MediaQuery.of(context).size.width : 0,
-            height: _shouldBlur ? MediaQuery.of(context).size.height : 0,
-            child: AnimatedOpacity(
-              child: ClipRect(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(
-                    sigmaX: 5.0,
-                    sigmaY: 5.0,
-                  ),
-                  child: GestureDetector(
-                    onTap: () => _closeSheet(),
-                    behavior: HitTestBehavior.translucent,
-                    child: Container(
-                      color: Colors.black.withOpacity(0.5),
-                    ),
+                              )
+                            : Center(child: CircularProgressIndicator()),
                   ),
                 ),
               ),
-              opacity: _shouldBlur ? 1 : 0,
-              duration: Duration(milliseconds: 300),
-            ),
+            ],
           ),
-
-          // Sheet for channel/direct adding
-          BlocConsumer<SheetBloc, SheetState>(
-            listener: (context, state) {
-              if (state is SheetShouldOpen) {
-                _openSheet();
-              }
-              if (state is SheetShouldClose) {
-                _closeSheet();
-              }
-            },
-            builder: (context, state) {
-              return SlideTransition(
-                position: _tween.animate(_animationController),
-                child: DraggableScrollable(),
-              );
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
+
+  // // Sheet for channel/direct adding
+  // BlocConsumer<SheetBloc, SheetState>(
+  // listener: (context, state) {
+  // if (state is SheetShouldOpen) {
+  // _openSheet();
+  // }
+  // if (state is SheetShouldClose) {
+  // _closeSheet();
+  // }
+  // },
+  // builder: (context, state) {
+  // return SlideTransition(
+  // position: _tween.animate(_animationController),
+  // child: DraggableScrollable(),
+  // );
+  // },
+  // ),
 
   void _openSheet() {
     if (_animationController.isDismissed) {
