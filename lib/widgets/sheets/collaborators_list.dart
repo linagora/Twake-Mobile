27a630894f59646e18bb4b065afbbcc5039609c7
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-02-04 16:34:44 +0300

    Workspace creation flow layout update.

--- lib/widgets/sheets/collaborators_list.dart
+++ lib/widgets/sheets/collaborators_list.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:twake/blocs/add_workspace_cubit/add_workspace_cubit.dart';
+import 'package:twake/blocs/fields_cubit/fields_cubit.dart';
+import 'package:twake/blocs/fields_cubit/fields_state.dart';
+import 'package:twake/repositories/add_workspace_repository.dart';
+import 'package:twake/widgets/sheets/sheet_title_bar.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CollaboratorsList extends StatelessWidget {
+class CollaboratorsList extends StatefulWidget {
+  @override
+  _CollaboratorsListState createState() => _CollaboratorsListState();
+}
+
+class _CollaboratorsListState extends State<CollaboratorsList> {
+  var _canInvite = false;
+
+  void _return() {
+    FocusScope.of(context).requestFocus(new FocusNode());
+    context.read<AddWorkspaceCubit>().setFlowStage(FlowStage.info);
+  }
+
+  void _invite() {
+    FocusScope.of(context).requestFocus(new FocusNode());
+    context.read<AddWorkspaceCubit>().setFlowStage(FlowStage.info);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        SheetTitleBar(
+          title: 'Invite',
+          leadingTitle: 'Back',
+          leadingAction: () => _return(),
+          trailingTitle: 'Invite',
+          trailingAction: _canInvite ? () => _invite() : null,
+        ),
+      ],
+    );
   }
 }
