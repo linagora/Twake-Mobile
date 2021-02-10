b09d8b38748aa6394b993e6a0afc1498202efadf
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-02-10 15:04:03 +0300

    Members fetch request added.

--- lib/repositories/channel_repository.dart
+++ lib/repositories/channel_repository.dart
@@ -142,7 +142,7 @@ class ChannelRepository {
       'channel_id': channelId,
       'members': members,
     };
-    _logger.d('Member update request body: $body');
+    _logger.d('Members update request body: $body');
     Map<String, dynamic> resp;
     try {
       resp = await _api.post(Endpoint.channelMembers, body: body);
@@ -153,6 +153,30 @@ class ChannelRepository {
     _logger.d('RESPONSE AFTER MEMBERS UPDATE: $resp');
     return true;
   }
+
+  Future<bool> fetchMembers({
+    @required String channelId,
+  }) async {
+    String companyId = ProfileBloc.selectedCompany;
+    String workspaceId = ProfileBloc.selectedWorkspace;
+
+    final queryParams = <String, dynamic>{
+      'company_id': companyId,
+      'workspace_id': workspaceId,
+      'channel_id': channelId,
+    };
+    _logger.d('Members fetch query parameters: $queryParams');
+    Map<String, dynamic> resp;
+    try {
+      resp = await _api.get(Endpoint.channelMembers, params: queryParams);
+    } catch (error) {
+      _logger.e('Error while trying to fetch members of a channel: $error');
+      return false;
+    }
+    _logger.d('RESPONSE AFTER MEMBERS FETCH: $resp');
+    return true;
+  }
+
 
   Future<String> process(Map<String, dynamic> body) async {
     _logger.d('Channel creation request body: $body');
