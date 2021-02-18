5964087208941d1cec9037df66c485cff5bcfff0
Makhmudov Babur <bobs4462@gmail.com>
2021-02-18 20:20:56 +0300

    Bug fixes for internet connection loss cases

--- lib/repositories/auth_repository.g.dart
+++ lib/repositories/auth_repository.g.dart
@@ -17,7 +17,10 @@ AuthRepository _$AuthRepositoryFromJson(Map<String, dynamic> json) {
     ..accessToken = json['token'] as String
     ..refreshToken = json['refresh_token'] as String
     ..accessTokenExpiration = json['expiration'] as int
-    ..refreshTokenExpiration = json['refresh_expiration'] as int;
+    ..refreshTokenExpiration = json['refresh_expiration'] as int
+    ..twakeConsole = json['twake_console'] as String
+    ..authMode = json['auth_mode'] as String
+    ..socketIOHost = json['socket_io_host'] as String;
 }
 
 Map<String, dynamic> _$AuthRepositoryToJson(AuthRepository instance) =>
@@ -26,4 +29,7 @@ Map<String, dynamic> _$AuthRepositoryToJson(AuthRepository instance) =>
       'refresh_token': instance.refreshToken,
       'expiration': instance.accessTokenExpiration,
       'refresh_expiration': instance.refreshTokenExpiration,
+      'twake_console': instance.twakeConsole,
+      'auth_mode': instance.authMode,
+      'socket_io_host': instance.socketIOHost,
     };
