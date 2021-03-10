544213032606492018825dd4a02a482f24336ef6
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-03-10 15:02:09 +0300

    CupertinoWarning text layout update. RichText implemented.

---
+++ lib/widgets/common/rich_text_span.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+
+class RichTextSpan extends TextSpan {
+  final String text;
+  final bool isBold;
+
+  const RichTextSpan({@required this.text, this.isBold = false});
+
+  TextSpan buildSpan() {
+    return TextSpan(
+      text: text,
+      style: isBold
+          ? TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.w700,
+              color: CupertinoColors.black,
+            )
+          : TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.w400,
+              color: CupertinoColors.black,
+            ),
+    );
+  }
+}
