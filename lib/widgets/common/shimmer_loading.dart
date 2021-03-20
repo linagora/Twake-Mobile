16c657c4b4638eadbf7b2508b68507b22f00c0ed
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-03-20 19:16:12 +0300

    Refactoring.

--- lib/widgets/common/shimmer_loading.dart
+++ lib/widgets/common/shimmer_loading.dart
@@ -4,24 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:meta/meta.dart';
 
-const _shimmerGradient = LinearGradient(
-  colors: [
-    // Colors.red,
-    // Colors.redAccent,
-    // Colors.green,
-    Color(0xFFEBEBF4),
-    Color(0xFFF4F4F4),
-    Color(0xFFEBEBF4),
-  ],
-  stops: [
-    0.1,
-    0.3,
-    0.4,
-  ],
-  begin: Alignment(-1.0, -0.3),
-  end: Alignment(1.0, 0.3),
-  tileMode: TileMode.clamp,
-);
 
 class ShimmerLoading extends StatefulWidget {
   const ShimmerLoading({
