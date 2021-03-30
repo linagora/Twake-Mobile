af6aba7cd8d5607eabf2d194c20ba6d78dc7381c
Makhmudov Babur <bobs4462@gmail.com>
2021-03-30 15:37:06 +0300

    Another iteration to write a fast parser, but unsuccessfully :(

--- lib/utils/twacode.dart
+++ lib/utils/twacode.dart
@@ -1,49 +1,114 @@
+import 'dart:math';
+
 class TwacodeParser {
   final String original;
-  Map<String, dynamic> twacode;
 
-  TwacodeParser(this.original);
+  List<ASTNode> nodes = [];
 
-  static final RegExp regexEmoji = RegExp(
-      r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])');
-  // for (var e in REGEX_EMOJI.allMatches(text)) {}
+  TwacodeParser(this.original) {
+    parse();
+  }
 
-  ASTNode parse(String text, [TwacodeType parent = TwacodeType.Root]) {
-    ASTNode node = ASTNode();
-    String running = "";
-
-    for (int i = 0; i < text.length; i++) {
-      var rune = text[i];
+  void parse() {
+    int start = 0;
+    for (var i = 0; i < original.length; i++) {
+      final rune = original[i];
       if (rune == Delimiter.star) {
-        if (parent == TwacodeType.Italic) {}
-      } else if (rune == Delimiter.underline) {
-        break;
-      } else if (rune == Delimiter.tilde) {
-        break;
-      } else if (rune == Delimiter.gt) {
-        break;
-      } else if (rune == Delimiter.tick) {
-        break;
-      } else if (rune == Delimiter.at) {
-        break;
+        print("GOT STAR");
+        final next = original[i + 1];
+        if (next == Delimiter.star) {
+          print("GOT ANOTHER STAR");
+          bool complete = false;
+          int endline = i + 2;
+          for (var j = i + 2;
+              j < original.length - 1 && original[j + 1] != '\n';
+              endline = ++j) {
+            if (original[j] == Delimiter.star &&
+                original[j + 1] == Delimiter.star) {
+              print("CLOSING BOLD");
+              if (i != 0) {
+                nodes.add(
+                  ASTNode(
+                    type: TwacodeType.Text,
+                    text: original.substring(start, i),
+                  ),
+                );
+              }
+              nodes.add(
+                ASTNode(
+                  type: TwacodeType.Bold,
+                  text: original.substring(i + 2, j),
+                ),
+              );
+              complete = true;
+              i = j + 2;
+              start = j + 2;
+              break;
+            }
+          }
+          if (!complete) {
+            nodes.add(
+              ASTNode(
+                type: TwacodeType.Text,
+                text: original.substring(start, endline),
+              ),
+            );
+            i = start = endline;
+          }
+        } else {
+          for (var j = i + 1;
+              j < original.length - 1 && original[j + 1] != '\n';
+              endline = ++j) {
+            if (original[j] == Delimiter.star &&
+                original[j + 1] == Delimiter.star) {
+              print("CLOSING BOLD");
+              if (i != 0) {
+                nodes.add(
+                  ASTNode(
+                    type: TwacodeType.Text,
+                    text: original.substring(start, i),
+                  ),
+                );
+              }
+              nodes.add(
+                ASTNode(
+                  type: TwacodeType.Bold,
+                  text: original.substring(i + 2, j),
+                ),
+              );
+              complete = true;
+              i = j + 2;
+              start = j + 2;
+              break;
+            }
+          }
+          if (!complete) {
+            nodes.add(
+              ASTNode(
+                type: TwacodeType.Text,
+                text: original.substring(start, endline),
+              ),
+            );
+            i = start = endline;
+          }
+        }
       }
-      running += rune;
     }
-    return node;
+  }
+
+  bool hasTripleTick(String line) {
+    return line.contains('```');
   }
 }
 
 class ASTNode {
   TwacodeType type;
-  List<ASTNode> children;
   String text;
+  ASTNode({this.type, this.text});
 
   dynamic transform() {
     Map<String, dynamic> map = {};
     switch (this.type) {
-      case TwacodeType.Root:
-        return this.children.map((c) => c.transform()).toList();
-
       case TwacodeType.Text:
         return this.text;
 
@@ -68,30 +133,30 @@ class ASTNode {
       case TwacodeType.Underline:
         map['start'] = '__';
         map['end'] = '__';
-        map['content'] = children.map((c) => c.transform()).toList();
+        map['content'] = this.text;
         break;
 
       case TwacodeType.StrikeThrough:
         map['start'] = '~~';
         map['end'] = '~~';
-        map['content'] = children.map((c) => c.transform()).toList();
+        map['content'] = this.text;
         break;
 
       case TwacodeType.Bold:
         map['start'] = '**';
         map['end'] = '**';
-        map['content'] = children.map((c) => c.transform()).toList();
+        map['content'] = this.text;
         break;
 
       case TwacodeType.Italic:
         map['start'] = '_';
         map['end'] = '_';
-        map['content'] = children.map((c) => c.transform()).toList();
+        map['content'] = this.text;
         break;
 
       case TwacodeType.Quote:
         map['start'] = '>';
-        map['content'] = children.map((c) => c.transform()).toList();
+        map['content'] = this.text;
         break;
 
       case TwacodeType.User:
@@ -115,8 +180,9 @@ class ASTNode {
         break;
 
       default:
-        throw Exception('Unsupported type');
+        throw Exception('Unsupported twacode type');
     }
+    return map;
   }
 }
 
@@ -144,4 +210,11 @@ class Delimiter {
   static String gt = '>';
   static String tick = '`';
   static String at = '@';
+  static String lf = '\n';
+}
+
+int main() {
+  final parsed = TwacodeParser("**HELLO * HELLO ** OUCH ** I *\n");
+  print("NODES: ${parsed.nodes.map((el) => el.transform())}");
+  return 1;
 }
