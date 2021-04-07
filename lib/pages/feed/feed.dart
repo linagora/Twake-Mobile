567803df2ec50ae74fc1c6bda9067c5c06f3c2fd
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-04-07 21:10:59 +0300

    Working on TabBarController.

--- lib/pages/feed/feed.dart
+++ lib/pages/feed/feed.dart
@@ -3,31 +3,6 @@ import 'package:flutter/material.dart';
 class Feed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  text: 'Channels',
-                ),
-                Tab(
-                  text: 'Direct chats',
-                ),
-              ],
-            ),
-            title: Text('Tabs Demo'),
-          ),
-          body: TabBarView(
-            children: [
-              Container(color: Colors.red),
-              Container(color: Colors.blue),
-            ],
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
