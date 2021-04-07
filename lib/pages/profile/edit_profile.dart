1b04fa654f911ffc949b826f1ab7e0376c91c840
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-04-07 18:02:38 +0300

    EditProfile layout update.

--- lib/pages/profile/edit_profile.dart
+++ lib/pages/profile/edit_profile.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:twake/blocs/profile_bloc/profile_bloc.dart';
-import 'package:twake/blocs/sheet_bloc/sheet_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:twake/widgets/common/selectable_avatar.dart';
-import 'package:twake/widgets/common/button_field.dart';
+import 'package:twake/widgets/common/rounded_text_field.dart';
 
 class EditProfile extends StatefulWidget {
   @override
@@ -16,6 +15,14 @@ class _EditProfileState extends State<EditProfile> {
   final _emailController = TextEditingController();
   final _cellularController = TextEditingController();
 
+  var _canSave = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController.text = 'Diana Potokina';
+  }
+
   @override
   void dispose() {
     _nameController.dispose();
@@ -23,6 +30,10 @@ class _EditProfileState extends State<EditProfile> {
     _emailController.dispose();
     _cellularController.dispose();
     super.dispose();
+  }
+
+  void _save() {
+    print('Save profile!');
   }
 
   @override
@@ -56,48 +67,59 @@ class _EditProfileState extends State<EditProfile> {
                       ),
                     ),
                   ),
-                  Column(
-                    children: [
-                      SelectableAvatar(
-                        size: 60.0,
-                        icon: '',
-                        onTap: () {},
+                  // SizedBox(width: 24.0),
+                  GestureDetector(
+                    onTap: _canSave ? () => _save() : null,
+                    child: Text(
+                      'Save',
+                      style: TextStyle(
+                        color: _canSave ? Color(0xff3840f7) : Color(0xffa2a2a2),
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w500,
                       ),
-                      SizedBox(height: 12.0),
-                      GestureDetector(
-                        onTap: () => print('Change avatar!'),
-                        child: Text('Tap to upload',
-                            style: TextStyle(
-                              color: Color(0xff3840f7),
-                              fontSize: 13.0,
-                              fontWeight: FontWeight.w400,
-                            )),
-                      ),
-                    ],
+                    ),
                   ),
-                  SizedBox(width: 24.0),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SelectableAvatar(
+                    size: 100.0,
+                    icon: '',
+                    onTap: () {},
+                  ),
+                  SizedBox(height: 12.0),
+                  GestureDetector(
+                    onTap: () => print('Change avatar!'),
+                    child: Text(
+                      'Tap to upload',
+                      style: TextStyle(
+                        color: Color(0xff3840f7),
+                        fontSize: 13.0,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
             SizedBox(height: 36.0),
             Text(
-              'Profile',
+              'NAME',
               style: TextStyle(
                 fontSize: 17.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                color: Colors.black.withOpacity(0.35),
               ),
             ),
             SizedBox(height: 12.0),
-            ButtonField(
-              title: 'Edit personal information',
-              titleStyle: TextStyle(
-                fontSize: 17.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.black,
-              ),
-              hasArrow: true,
-              arrowColor: Color(0xff3c3c43).withOpacity(0.3),
+            RoundedTextField(
+              controller: _nameController,
+              hint: 'Not assigned',
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(10.0),
                 topRight: Radius.circular(10.0),
@@ -107,71 +129,85 @@ class _EditProfileState extends State<EditProfile> {
               height: 1.0,
               color: Colors.black.withOpacity(0.1),
             ),
-            ButtonField(
-              title: 'Privacy settings',
-              titleStyle: TextStyle(
-                fontSize: 17.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.black,
-              ),
-              hasArrow: true,
-              arrowColor: Color(0xff3c3c43).withOpacity(0.3),
+            RoundedTextField(
+              controller: _displayedNameController,
+              hint: 'Not assigned',
               borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(10.0),
                 bottomRight: Radius.circular(10.0),
               ),
             ),
-            SizedBox(height: 40.0),
+            SizedBox(height: 8.0),
+            Center(
+              child: Text(
+                '«Displayed name» is how people see your name in Twake\nin @mentions and by other users in channels and private chats',
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 11.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black.withOpacity(0.35),
+                ),
+              ),
+            ),
+            SizedBox(height: 43.0),
             Text(
-              'Companies and workspaces',
+              'CONTACT DETAILS',
               style: TextStyle(
                 fontSize: 17.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                color: Colors.black.withOpacity(0.35),
               ),
             ),
             SizedBox(height: 12.0),
-            ButtonField(
-              title: 'Active companies',
-              titleStyle: TextStyle(
-                fontSize: 17.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.black,
+            RoundedTextField(
+              controller: _nameController,
+              hint: 'Not assigned',
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
               ),
-              trailingTitle: '5',
-              trailingTitleStyle: TextStyle(
-                fontSize: 15.0,
-                fontWeight: FontWeight.w600,
-                color: Color(0xff004dff),
-              ),
-              hasArrow: true,
-              arrowColor: Color(0xff3c3c43).withOpacity(0.3),
             ),
-            SizedBox(height: 32.0),
+            Divider(
+              height: 1.0,
+              color: Colors.black.withOpacity(0.1),
+            ),
+            RoundedTextField(
+              controller: _displayedNameController,
+              hint: 'Not assigned',
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0),
+              ),
+            ),
+            SizedBox(height: 24.0),
             Text(
-              'Users',
+              'DATE & TIME',
               style: TextStyle(
                 fontSize: 17.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                color: Colors.black.withOpacity(0.35),
               ),
             ),
             SizedBox(height: 12.0),
-            ButtonField(
-              title: 'Manage users',
-              titleStyle: TextStyle(
-                fontSize: 17.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.black,
+            RoundedTextField(
+              controller: _nameController,
+              hint: 'Not assigned',
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
               ),
-              trailingTitle: '124',
-              trailingTitleStyle: TextStyle(
-                fontSize: 15.0,
-                fontWeight: FontWeight.w600,
-                color: Color(0xff004dff),
+            ),
+            Divider(
+              height: 1.0,
+              color: Colors.black.withOpacity(0.1),
+            ),
+            RoundedTextField(
+              controller: _displayedNameController,
+              hint: 'Not assigned',
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0),
               ),
-              hasArrow: true,
-              arrowColor: Color(0xff3c3c43).withOpacity(0.3),
             ),
           ],
         ),
--- lib/widgets/common/rounded_text_field.dart
+++ lib/widgets/common/rounded_text_field.dart
@@ -4,29 +4,33 @@ import 'package:flutter/services.dart';
 
 class RoundedTextField extends StatelessWidget {
   final String hint;
-  final Function leadingAction;
-  final Function trailingAction;
+  final String prefix;
   final TextEditingController controller;
   final FocusNode focusNode;
   final int maxLength;
   final String Function(String) validator;
+  final BorderRadius borderRadius;
 
   const RoundedTextField({
     Key key,
     this.hint,
-    this.leadingAction,
-    this.trailingAction,
+    this.prefix,
     this.controller,
     this.focusNode,
     this.maxLength,
     this.validator,
+    this.borderRadius = BorderRadius.zero,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(left: 14.0, right: 7),
-      color: Colors.white,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: borderRadius != BorderRadius.zero
+            ? borderRadius
+            : BorderRadius.circular(10.0),
+      ),
       child: TextFormField(
         inputFormatters: [
           LengthLimitingTextInputFormatter(maxLength),
@@ -35,6 +39,7 @@ class RoundedTextField extends StatelessWidget {
         controller: controller,
         focusNode: focusNode,
         keyboardType: TextInputType.emailAddress,
+        textAlign: TextAlign.end,
         style: TextStyle(
           fontSize: 17.0,
           fontWeight: FontWeight.w400,
@@ -42,63 +47,16 @@ class RoundedTextField extends StatelessWidget {
         ),
         decoration: InputDecoration(
           hintText: hint,
+          prefixText: prefix,
           hintStyle: TextStyle(
             fontSize: 17.0,
             fontWeight: FontWeight.w400,
             color: Color(0xffc8c8c8),
           ),
           alignLabelWithHint: true,
-          // contentPadding: widget.leadingAction != null
-          //     ? EdgeInsets.only(left: 14, bottom: 5)
-          //     : null,
-          contentPadding: EdgeInsets.only(left: 0),
+          contentPadding: EdgeInsets.only(left: 0, right: 16.0),
           fillColor: Colors.transparent,
           filled: true,
-          prefix: (leadingAction != null && focusNode.hasFocus)
-              ? Container(
-                  width: 30,
-                  height: 25,
-                  padding: EdgeInsets.only(right: 10),
-                  child: IconButton(
-                    onPressed: leadingAction,
-                    padding: EdgeInsets.all(0),
-                    iconSize: 20,
-                    icon: Icon(
-                      CupertinoIcons.minus_circle_fill,
-                      color: Color(0xfff14620),
-                    ),
-                  ),
-                )
-              : null,
-          suffix: trailingAction != null
-              ? Container(
-                  width: 30,
-                  height: 25,
-                  padding: EdgeInsets.only(left: 10),
-                  child: IconButton(
-                    onPressed: trailingAction,
-                    padding: EdgeInsets.all(0),
-                    iconSize: 20,
-                    icon: Icon(
-                      CupertinoIcons.add_circled,
-                      color: Color(0xff3840F7),
-                    ),
-                  ),
-                )
-              : Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Color(0xffeeeeef),
-                  ),
-                  width: 30,
-                  height: 20,
-                  child: IconButton(
-                    padding: EdgeInsets.all(0),
-                    onPressed: () => controller.clear(),
-                    iconSize: 15,
-                    icon: Icon(CupertinoIcons.clear),
-                  ),
-                ),
           border: UnderlineInputBorder(
             borderSide: BorderSide(
               width: 0.0,
