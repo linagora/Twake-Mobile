2a2544a64766bc04203781dddab80f1e295347cb
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-04-08 11:03:16 +0300

    Channels page update.

--- lib/pages/feed/channels.dart
+++ lib/pages/feed/channels.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
 
-class Channels extends StatelessWidget {
+class Channels extends StatefulWidget {
+  @override
+  _ChannelsState createState() => _ChannelsState();
+}
+
+class _ChannelsState extends State<Channels> {
+  var _selectedTab = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: DefaultTabController(
         length: 2,
         child: Scaffold(
+          primary: false,
           appBar: AppBar(
             bottom: TabBar(
+              onTap: (index) {
+                setState(() {
+                  _selectedTab = index;
+                });
+              },
+              indicatorPadding: _selectedTab == 0
+                  ? EdgeInsets.only(left: 30.0)
+                  : EdgeInsets.only(right: 30.0),
+              indicatorColor: Color(0xff004dff),
+              indicatorSize: TabBarIndicatorSize.label,
               tabs: [
-                Tab(
-                  text: 'Channels',
+                Container(
+                  width: 130.0,
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Tab(
+                    text: 'Channels',
+                  ),
                 ),
-                Tab(
-                  text: 'Direct chats',
+                Container(
+                  width: 130.0,
+                  padding: const EdgeInsets.only(right: 30.0),
+                  child: Tab(
+                    text: 'Direct chats',
+                  ),
                 ),
               ],
             ),
