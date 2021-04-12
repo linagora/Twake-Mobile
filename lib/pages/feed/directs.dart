c4f6d1cf3856518e675e730cd578807128ab97a7
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-04-12 11:19:30 +0300

    Working on Directs.

--- lib/pages/feed/directs.dart
+++ lib/pages/feed/directs.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:twake/blocs/directs_bloc/directs_bloc.dart';
+import 'package:twake/models/direct.dart';
+import 'package:twake/pages/feed/channel_tile.dart';
 
 class Directs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.white54);
+    return BlocBuilder<DirectsBloc, ChannelState>(
+      buildWhen: (_, current) =>
+      current is ChannelsLoading ||
+          current is ChannelsLoaded ||
+          current is ChannelsEmpty,
+      builder: (context, state) {
+        var channels = <Direct>[];
+        if (state is ChannelsLoaded) {
+          channels = state.channels;
+        }
+        return Container(
+          child: ListView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsets.only(top: 12.0),
+            itemCount: channels.length,
+            itemBuilder: (context, index) {
+              final channel = channels[index];
+              return ChannelTile(
+                id: channel.id,
+                name: channel.name,
+                icon: channel.icon,
+                hasUnread: channel.hasUnread == 1,
+                isPrivate: false,
+                lastActivity: channel.lastActivity,
+                lastMessage: channel.lastMessage,
+                messagesUnread: channel.messagesUnread,
+              );
+            },
+          ),
+        );
+      },
+    );
   }
 }
