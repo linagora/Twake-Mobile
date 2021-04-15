a443ba16ff03651fe132bdb44c45685fcf35fb22
Pavel Zarudnev <rockinpaulz@gmail.com>
2021-04-15 19:45:06 +0300

    Working on bubbles implementation.

--- lib/pages/chat/message_tile.dart
+++ lib/pages/chat/message_tile.dart
@@ -1,3 +1,4 @@
+import 'package:bubble/bubble.dart';
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -86,7 +87,7 @@ class _MessageTileState<T extends BaseChannelBloc>
       lazy: false,
       child: BlocBuilder<SingleMessageBloc, SingleMessageState>(
         builder: (context, messageState) {
-          if (messageState is MessageReady)
+          if (messageState is MessageReady) {
             return InkWell(
               onLongPress: () {
                 BlocProvider.of<MessageEditBloc>(context)
@@ -172,76 +173,81 @@ class _MessageTileState<T extends BaseChannelBloc>
                     ),
                     SizedBox(width: 12),
                     Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                messageState.sender ?? '',
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.w500,
-                                  color: Color(0xff444444),
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              Text(
-                                messageState.threadId != null ||
-                                        _hideShowAnswers
-                                    ? DateFormatter.getVerboseDateTime(
-                                        messageState.creationDate)
-                                    : DateFormatter.getVerboseTime(
-                                        messageState.creationDate),
-                                style: TextStyle(
-                                  fontSize: 11.0,
-                                  fontWeight: FontWeight.w400,
-                                  color: Color(0xff92929C),
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 5.0),
-                          TwacodeRenderer(messageState.content).message,
-                          // Normally we use SizedBox here,
-                          // but it will cut the bottom of emojis
-                          // in last line of the messsage.
-                          Container(
-                            color: Colors.transparent,
-                            height: 5.0,
-                          ),
-                          Wrap(
-                            runSpacing: Dim.heightMultiplier,
-                            crossAxisAlignment: WrapCrossAlignment.center,
-                            textDirection: TextDirection.ltr,
-                            children: [
-                              ...messageState.reactions.keys.map((r) {
-                                return Reaction(
-                                  r,
-                                  messageState.reactions[r]['count'],
-                                  T == DirectsBloc ? 'direct' : null,
-                                );
-                              }),
-                              if (messageState.responsesCount > 0 &&
-                                  messageState.threadId == null &&
-                                  !_hideShowAnswers)
+                      child: Bubble(
+                        color: Color(0xfff6f6f6),
+                        elevation: 0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
                                 Text(
-                                  'See all answers (${messageState.responsesCount})',
-                                  style: StylesConfig.miniPurple,
+                                  messageState.sender ?? '',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xff444444),
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
                                 ),
-                            ],
-                          ),
-                        ],
+                                Text(
+                                  messageState.threadId != null ||
+                                          _hideShowAnswers
+                                      ? DateFormatter.getVerboseDateTime(
+                                          messageState.creationDate)
+                                      : DateFormatter.getVerboseTime(
+                                          messageState.creationDate),
+                                  style: TextStyle(
+                                    fontSize: 11.0,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xff92929C),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 5.0),
+                            TwacodeRenderer(messageState.content).message,
+                            // Normally we use SizedBox here,
+                            // but it will cut the bottom of emojis
+                            // in last line of the messsage.
+                            Container(
+                              color: Colors.transparent,
+                              height: 5.0,
+                            ),
+                            Wrap(
+                              runSpacing: Dim.heightMultiplier,
+                              crossAxisAlignment: WrapCrossAlignment.center,
+                              textDirection: TextDirection.ltr,
+                              children: [
+                                ...messageState.reactions.keys.map((r) {
+                                  return Reaction(
+                                    r,
+                                    messageState.reactions[r]['count'],
+                                    T == DirectsBloc ? 'direct' : null,
+                                  );
+                                }),
+                                if (messageState.responsesCount > 0 &&
+                                    messageState.threadId == null &&
+                                    !_hideShowAnswers)
+                                  Text(
+                                    'See all answers (${messageState.responsesCount})',
+                                    style: StylesConfig.miniPurple,
+                                  ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
             );
-          else
+          } else {
             return CircularProgressIndicator();
+          }
         },
       ),
     );
