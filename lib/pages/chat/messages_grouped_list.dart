4e4763e648944e0bcdb0dda64784d86d00ac6df6
Manh Duc NGUYEN <mdnguyen@linagora.com>
2021-04-22 15:00:40 +0700

    [Bug-475] When open an unread messages list, it doesn't focus on the first unread message

--- lib/pages/chat/messages_grouped_list.dart
+++ lib/pages/chat/messages_grouped_list.dart
@@ -5,6 +5,7 @@ import 'package:sticky_grouped_list/sticky_grouped_list.dart';
 import 'package:twake/blocs/base_channel_bloc/base_channel_bloc.dart';
 import 'package:twake/blocs/messages_bloc/messages_bloc.dart';
 import 'package:twake/blocs/messages_bloc/messsage_loaded_type.dart';
+import 'package:twake/blocs/profile_bloc/profile_bloc.dart';
 import 'package:twake/blocs/single_message_bloc/single_message_bloc.dart';
 import 'package:twake/models/direct.dart';
 import 'package:twake/pages/chat/empty_chat_container.dart';
@@ -70,8 +71,7 @@ class _MessagesGroupedListState<T extends BaseChannelBloc>
     );
   }
 
-  Widget _buildStickyGroupedListView(
-      BuildContext context, MessagesState state, List<Message> messages) {
+  Widget _buildStickyGroupedListView(BuildContext context, MessagesState state, List<Message> messages) {
     var lastScrollPosition = 0;
     try {
       if (state is MessagesLoaded) {
@@ -79,6 +79,12 @@ class _MessagesGroupedListState<T extends BaseChannelBloc>
           lastScrollPosition = _itemPositionListener.itemPositions.value.last.index;
         } else if (state.messageLoadedType == MessageLoadedType.afterDelete) {
           lastScrollPosition = _itemPositionListener.itemPositions.value.first.index;
+        } else {
+          final profileState = context.read<ProfileBloc>().state;
+          if (profileState is ProfileLoaded) {
+            final badge = profileState.getBadgeForChannel(state.parentChannel.id);
+            lastScrollPosition = badge > 1 ? badge : 0;
+          }
         }
       }
     } catch (exception) {
