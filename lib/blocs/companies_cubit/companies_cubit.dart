a4efa6cd61129e5ee15c2ddd7023adabb077acc1
nmduc <manhducnguyen.it@gmail.com>
2021-05-28 13:49:56 +0700

    Add companies_cubit + companies_repository

--- lib/blocs/companies_cubit/companies_cubit.dart
+++ lib/blocs/companies_cubit/companies_cubit.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:twake/models/company/company.dart';
 import 'package:twake/models/globals/globals.dart';
 import 'package:twake/repositories/companies_repository.dart';
 
@@ -9,24 +8,21 @@ import 'companies_state.dart';
 
 class CompaniesCubit extends Cubit<CompaniesState> {
   final CompanyRepository repository;
-  List<Company> _companies = [];
 
   CompaniesCubit(this.repository) : super(CompaniesInitial());
 
   Future<void> fetch() async {
     emit(CompaniesLoadInProgress());
-    final companies = await repository.fetch();
-    _companies = companies;
-    emit(CompaniesLoadSuccess(
-      companies: companies,
-    ));
+    final streamCompanies = repository.fetch();
+
+    await for (var companies in streamCompanies) {
+      emit(CompaniesLoadSuccess(
+        companies: companies,
+      ));
+    }
   }
 
   void selectCompany(String companyId) {
     Globals.instance.companyIdSet = companyId;
-    final newState = CompaniesLoadSuccess(
-      companies: _companies,
-    );
-    emit(newState);
   }
 }
