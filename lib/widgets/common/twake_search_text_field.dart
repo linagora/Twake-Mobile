f7086df9eddf4624503080c1dcda268b59fbbe91
nmduc <mdnguyen@linagora.com>
2021-07-02 15:19:27 +0700

    add clear function to twake_search_text_field

--- lib/widgets/common/twake_search_text_field.dart
+++ lib/widgets/common/twake_search_text_field.dart
@@ -1,38 +1,68 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TwakeSearchTextField extends StatelessWidget {
+class TwakeSearchTextField extends StatefulWidget {
   final double height;
   final String hintText;
   final Color backgroundColor;
   final double fontSize;
   final bool showPrefixIcon;
-  final TextEditingController? controller;
+  late final TextEditingController? controller;
 
-  const TwakeSearchTextField(
+  TwakeSearchTextField(
       {this.height = 46,
-        this.hintText = 'Search',
-        this.backgroundColor = const Color(0xfff9f8f9),
-        this.fontSize = 17,
-        this.controller,
-        this.showPrefixIcon = true
-      }) : super();
+      this.hintText = 'Search',
+      this.backgroundColor = const Color(0xfff9f8f9),
+      this.fontSize = 17,
+      TextEditingController? controller,
+      this.showPrefixIcon = true}) : super() {
+    this.controller = controller != null ? controller : TextEditingController();
+  }
+
+  @override
+  _TwakeSearchTextFieldState createState() => _TwakeSearchTextFieldState();
+}
+
+class _TwakeSearchTextFieldState extends State<TwakeSearchTextField> {
+  var showClearButton = false;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.controller != null) {
+      widget.controller!.addListener(() {
+        setState(() {
+          showClearButton = widget.controller!.text.isNotEmpty;
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: height,
+      height: widget.height,
       child: TextField(
-        controller: controller,
+        controller: widget.controller,
         cursorColor: Color(0xff004dff),
         style: TextStyle(
           color: Colors.black,
-          fontSize: fontSize,
+          fontSize: widget.fontSize,
           fontWeight: FontWeight.w400,
           fontStyle: FontStyle.normal,
         ),
         decoration: new InputDecoration(
-          contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: showPrefixIcon ? 0 : 30),
-          prefixIcon: showPrefixIcon ? Icon(Icons.search) : null,
+          contentPadding: EdgeInsets.only(
+              top: 10, bottom: 10, left: widget.showPrefixIcon ? 0 : 30),
+          prefixIcon: widget.showPrefixIcon ? Icon(Icons.search) : null,
+          suffixIcon: showClearButton ? Padding(
+            padding: const EdgeInsets.only(right: 16.0, left: 16),
+            child: GestureDetector(
+                onTap: () {
+                  widget.controller?.clear();
+                }, child: _ClearIcon()),
+          ) : SizedBox.shrink(),
+          suffixIconConstraints: BoxConstraints(minHeight: 16, minWidth: 16),
           border: new OutlineInputBorder(
             borderRadius: const BorderRadius.all(
               const Radius.circular(10.0),
@@ -45,12 +75,33 @@ class TwakeSearchTextField extends StatelessWidget {
           filled: true,
           hintStyle: TextStyle(
             color: Color(0xff8e8e93),
-            fontSize: fontSize,
+            fontSize: widget.fontSize,
             fontWeight: FontWeight.w400,
             fontStyle: FontStyle.normal,
           ),
-          hintText: hintText,
-          fillColor: backgroundColor,
+          hintText: widget.hintText,
+          fillColor: widget.backgroundColor,
+        ),
+      ),
+    );
+  }
+}
+
+class _ClearIcon extends StatelessWidget {
+  const _ClearIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Container(
+        color: Colors.grey,
+        width: 16,
+        height: 16,
+        child: Icon(
+          Icons.close,
+          size: 12,
+          color: Colors.white,
         ),
       ),
     );
