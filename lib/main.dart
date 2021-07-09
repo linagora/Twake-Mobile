0ac5568fc57583a679cfba75af744df53944c4da
EvGen94 <sharanov_evgenii@mail.ru>
2021-07-09 19:16:27 +0300

    add ios requestPermission

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,17 @@ void main() async {
   await InitService.preAuthenticationInit();
 
   runApp(RefreshConfiguration(
-    headerBuilder: () => PullToRefreshHeader(height: 100, padding: EdgeInsets.only(top: 22)), // Configure the default header indicator. If you have the same header indicator for each page, you need to set this
+    headerBuilder: () => PullToRefreshHeader(
+        height: 100,
+        padding: EdgeInsets.only(
+            top:
+                22)), // Configure the default header indicator. If you have the same header indicator for each page, you need to set this
     headerTriggerDistance: 80.0, // header trigger refresh trigger distance
     maxUnderScrollExtent: 0, // Maximum dragging range at the bottom
-    enableScrollWhenRefreshCompleted: true, //This property is incompatible with PageView and TabBarView. If you need TabBarView to slide left and right, you need to set it to true.
-    enableLoadingWhenFailed : true, //In the case of load failure, users can still trigger more loads by gesture pull-up.
+    enableScrollWhenRefreshCompleted:
+        true, //This property is incompatible with PageView and TabBarView. If you need TabBarView to slide left and right, you need to set it to true.
+    enableLoadingWhenFailed:
+        true, //In the case of load failure, users can still trigger more loads by gesture pull-up.
     enableBallisticLoad: true, // trigger load more by BallisticScrollActivity
 
     child: GetMaterialApp(
