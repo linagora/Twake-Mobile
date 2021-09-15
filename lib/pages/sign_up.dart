322a3bd1b65a9c681e37d98fb364fee94fbfa974
Evgenii <51719953+EvGen94@users.noreply.github.com>
2021-09-15 19:01:12 +0400

    Add more ergonomic
    Add more ergonomic condition

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -47,10 +47,9 @@ class _SignUpState extends State<SignUp> {
   void _sendLink(String email) async {
     if (_formKey.currentState!.validate()) {
       final stateRegistration = Get.find<RegistrationCubit>().state;
-      if (stateRegistration is RegistrationFailed) {
-        if (stateRegistration.emailExists) {
-          Get.find<RegistrationCubit>().prepare();
-        }
+      if (stateRegistration is RegistrationFailed &&
+          stateRegistration.emailExists) {
+        Get.find<RegistrationCubit>().prepare();
       }
       if (stateRegistration is RegistrationReady) {
         await Get.find<RegistrationCubit>().signup(
