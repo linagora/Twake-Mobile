2b1290d884d50c95f812398af4179de5dc920f57
EvGen94 <sharanov_evgenii@mail.ru>
2021-11-30 17:50:12 +0300

    fix #1030 email textbox cannot display in a small screensize

--- lib/pages/workspaces/create_workspace.dart
+++ lib/pages/workspaces/create_workspace.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:twake/blocs/account_cubit/account_cubit.dart';
 import 'package:twake/blocs/channels_cubit/channels_cubit.dart';
 import 'package:twake/blocs/workspaces_cubit/workspaces_cubit.dart';
+import 'package:twake/config/dimensions_config.dart';
 import 'package:twake/models/account/account.dart';
 import 'package:twake/models/globals/globals.dart';
 import 'package:twake/routing/app_router.dart';
@@ -94,189 +95,184 @@ class _WorkspaceFormState extends State<WorkspaceForm> {
     });
   }
 
+  _createWorksapce() async {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          margin: EdgeInsets.fromLTRB(
+            15.0,
+            5.0,
+            15.0,
+            65.0,
+            //  Dim.heightPercent(8),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          behavior: SnackBarBehavior.floating,
+          duration: Duration(seconds: 3),
+          content: Text(AppLocalizations.of(context)!.processing),
+        ),
+      );
+
+      await Get.find<WorkspacesCubit>().createWorkspace(
+        companyId: Globals.instance.companyId,
+        name: _workspaceNameController.text,
+        members: _members,
+      );
+
+      Get.find<ChannelsCubit>().fetch(
+        workspaceId: Globals.instance.workspaceId!,
+        companyId: Globals.instance.companyId,
+      );
+
+      Navigator.of(context).pop();
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          margin: EdgeInsets.fromLTRB(
+            15.0,
+            5.0,
+            15.0,
+            65.0,
+            //  Dim.heightPercent(8),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          behavior: SnackBarBehavior.floating,
+          duration: Duration(seconds: 3),
+          content: Text(
+            AppLocalizations.of(context)!.workspaceCreationErrorInfo,
+          ),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          color: Color(0xFFF2F2F6),
-          child: Column(
-            children: [
-              Container(
-                color: Colors.white,
-                child: SheetTitleBar(
-                    title: AppLocalizations.of(context)!.newWorkspace,
-                    leadingTitle: AppLocalizations.of(context)!.cancel,
-                    leadingAction: () {
-                      FocusScope.of(context).requestFocus(new FocusNode());
-                      popBack();
-                    },
-                    trailingTitle: AppLocalizations.of(context)!.create,
-                    trailingAction: () async {
-                      if (_formKey.currentState!.validate()) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            margin: EdgeInsets.fromLTRB(
-                              15.0,
-                              5.0,
-                              15.0,
-                              65.0,
-                              //  Dim.heightPercent(8),
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            behavior: SnackBarBehavior.floating,
-                            duration: Duration(seconds: 3),
-                            content:
-                                Text(AppLocalizations.of(context)!.processing),
-                          ),
-                        );
-
-                        await Get.find<WorkspacesCubit>().createWorkspace(
-                          companyId: Globals.instance.companyId,
-                          name: _workspaceNameController.text,
-                          members: _members,
-                        );
-
-                        Get.find<ChannelsCubit>().fetch(
-                          workspaceId: Globals.instance.workspaceId!,
-                          companyId: Globals.instance.companyId,
-                        );
-
-                        Navigator.of(context).pop();
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            margin: EdgeInsets.fromLTRB(
-                              15.0,
-                              5.0,
-                              15.0,
-                              65.0,
-                              //  Dim.heightPercent(8),
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            behavior: SnackBarBehavior.floating,
-                            duration: Duration(seconds: 3),
-                            content: Text(
-                              AppLocalizations.of(context)!
-                                  .workspaceCreationErrorInfo,
-                            ),
-                          ),
-                        );
-                      }
-                    }),
-              ),
-              Flexible(
-                child: Container(
+        child: SingleChildScrollView(
+          child: Container(
+            constraints: BoxConstraints(minHeight: Dim.heightPercent(100)),
+            color: Color(0xFFF2F2F6),
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  child: SheetTitleBar(
+                      title: AppLocalizations.of(context)!.newWorkspace,
+                      leadingTitle: AppLocalizations.of(context)!.cancel,
+                      leadingAction: () {
+                        FocusScope.of(context).requestFocus(new FocusNode());
+                        popBack();
+                      },
+                      trailingTitle: AppLocalizations.of(context)!.create,
+                      trailingAction: () {
+                        _createWorksapce();
+                      }),
+                ),
+                Container(
                   height: 16,
                   constraints: BoxConstraints(minHeight: 2),
                 ),
-              ),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10, right: 10),
-                    child: Container(
-                      height: 80,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 15, right: 15),
-                        child: Container(
-                          child: Row(
-                            children: [
-                              GestureDetector(
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Color(0xFFF5F5F5),
-                                    borderRadius: BorderRadius.circular(10.0),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: Container(
+                        height: 80,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15, right: 15),
+                          child: Container(
+                            child: Row(
+                              children: [
+                                GestureDetector(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Color(0xFFF5F5F5),
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    child: Icon(
+                                      Icons.camera_alt_rounded,
+                                      color: Color(0xFF969CA4),
+                                    ),
+                                    height: 44,
+                                    width: 44,
                                   ),
-                                  child: Icon(
-                                    Icons.camera_alt_rounded,
-                                    color: Color(0xFF969CA4),
-                                  ),
-                                  height: 44,
-                                  width: 44,
+                                  onTap: () async {},
                                 ),
-                                onTap: () async {},
-                              ),
-                              Flexible(
-                                child: Center(
-                                  child: Form(
-                                    key: _formKey,
-                                    child: SheetTextField(
-                                      hint: AppLocalizations.of(context)!
-                                          .workspaceNameInfo,
-                                      controller: _workspaceNameController,
-                                      focusNode: _workspaceNameFocusNode,
-                                      maxLength: 30,
-                                      isRounded: true,
-                                      borderRadius: 8,
-                                      textInputType: TextInputType.text,
-                                      validator: (value) {
-                                        if (value == null || value.isEmpty) {
-                                          return AppLocalizations.of(context)!
-                                              .workspaceNameError;
-                                        }
-                                        return null;
-                                      },
+                                Flexible(
+                                  child: Center(
+                                    child: Form(
+                                      key: _formKey,
+                                      child: SheetTextField(
+                                        hint: AppLocalizations.of(context)!
+                                            .workspaceNameInfo,
+                                        controller: _workspaceNameController,
+                                        focusNode: _workspaceNameFocusNode,
+                                        maxLength: 30,
+                                        isRounded: true,
+                                        borderRadius: 8,
+                                        textInputType: TextInputType.text,
+                                        validator: (value) {
+                                          if (value == null || value.isEmpty) {
+                                            return AppLocalizations.of(context)!
+                                                .workspaceNameError;
+                                          }
+                                          return null;
+                                        },
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 12),
-                    child: HintLine(
-                      text: AppLocalizations.of(context)!
-                          .workspaceCreationErrorInfo,
-                      fontWeight: FontWeight.w500,
+                    SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: HintLine(
+                        text: AppLocalizations.of(context)!
+                            .workspaceCreationErrorInfo,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Flexible(
-                flex: 1,
-                child: Container(
+                  ],
+                ),
+                Container(
                   height: 40,
                   constraints: BoxConstraints(minHeight: 2),
                 ),
-              ),
-              HintLine(
-                text: AppLocalizations.of(context)!.addYourTeamMembers,
-                isLarge: true,
-                fontWeight: FontWeight.w500,
-              ),
-              Flexible(
-                flex: 1,
-                child: Container(
+                HintLine(
+                  text: AppLocalizations.of(context)!.addYourTeamMembers,
+                  isLarge: true,
+                  fontWeight: FontWeight.w500,
+                ),
+                Container(
                   height: 12,
                   constraints: BoxConstraints(minHeight: 3),
                 ),
-              ),
-              Flexible(
-                flex: 3,
-                child: ListView.builder(
+                ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
                   itemCount: _count,
                   itemBuilder: (context, index) {
                     return _textField(index, _controllers[index]);
                   },
                 ),
-              ),
-              _addMember(),
-            ],
+                _addMember(),
+              ],
+            ),
           ),
         ),
       ),
