8d3c60b2c8f9da04e049a00c3122a70bc70dfc62
Huy Nguyen <huynguyennovem@gmail.com>
2022-03-15 18:51:41 +0700

    966: File view in channel - discover files in thread

--- lib/models/channel/channel_file.dart
+++ lib/models/channel/channel_file.dart
@@ -3,9 +3,10 @@ import 'package:equatable/equatable.dart';
 class ChannelFile extends Equatable {
   final String fileId;
   final String senderName;
+  final int createdAt;
 
-  ChannelFile(this.fileId, this.senderName);
+  ChannelFile(this.fileId, this.senderName, this.createdAt);
 
   @override
-  List<Object> get props => [fileId, senderName];
+  List<Object> get props => [fileId, senderName, createdAt];
 }
