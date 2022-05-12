96b2fce9ca2918c845ca400f9419cbb7c5be81d2
sherlockvn <quangkhai28999@gmail.com>
2022-05-12 12:55:25 +0700

    add jumpMessage functionality and create a searchable_grouped_listview widget allow jump to specific message with animation

--- lib/pages/chat/messages_grouped_list.dart
+++ lib/pages/chat/messages_grouped_list.dart
@@ -3,17 +3,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:twake/blocs/channels_cubit/channels_cubit.dart';
 import 'package:twake/blocs/messages_cubit/messages_cubit.dart';
-import 'package:grouped_list/grouped_list.dart';
-import 'package:twake/blocs/messages_cubit/messages_state.dart';
 import 'package:twake/models/globals/globals.dart';
 import 'package:twake/models/message/message.dart';
 import 'package:twake/pages/chat/empty_chat_container.dart';
 import 'package:twake/pages/chat/message_tile.dart';
 import 'package:twake/services/navigator_service.dart';
 import 'package:twake/utils/bubble_side.dart';
-import 'package:twake/utils/dateformatter.dart';
 import 'package:flutter_swipe_action_cell/flutter_swipe_action_cell.dart';
 import 'package:twake/widgets/common/channel_first_message.dart';
+import 'package:twake/widgets/common/searchable_grouped_listview.dart';
 
 class MessagesGroupedList extends StatefulWidget {
   final Channel parentChannel;
@@ -25,7 +23,9 @@ class MessagesGroupedList extends StatefulWidget {
 
 class _MessagesGroupedListState extends State<MessagesGroupedList> {
   final SwipeActionController swipeController = SwipeActionController();
-  
+  final SearchableGroupChatController _controller =
+      SearchableGroupChatController();
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ChannelMessagesCubit, MessagesState>(
@@ -71,96 +71,58 @@ class _MessagesGroupedListState extends State<MessagesGroupedList> {
 
   Widget _buildStickyGroupedListView(
       BuildContext context, List<Message> messages, bool endOfHistory) {
-    return GroupedListView<Message, DateTime>(
-      // addAutomaticKeepAlives: true,
-      key: PageStorageKey<String>('uniqueKey'),
-      order: GroupedListOrder.DESC,
-      stickyHeaderBackgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      padding: EdgeInsets.only(bottom: 12.0),
-      reverse: true,
-      elements: messages,
-      groupBy: (Message m) {
-        final DateTime dt = DateTime.fromMillisecondsSinceEpoch(m.createdAt);
-        return DateTime(dt.year, dt.month, dt.day);
-      },
-      groupComparator: (DateTime value1, DateTime value2) =>
-          value1.compareTo(value2),
-      itemComparator: (Message m1, Message m2) {
-        if (m1.createdAt.compareTo(m2.createdAt) == -1) {
-          return m1.createdAt.compareTo(m2.createdAt);
-        } else {
-          return m2.createdAt.compareTo(m1.createdAt);
-        }
-      },
-      separator: SizedBox(height: 1.0),
-      groupSeparatorBuilder: (DateTime dt) {
-        return GestureDetector(
-          onTap: () {
-            FocusManager.instance.primaryFocus!.unfocus();
-          },
-          child: Container(
-            height: 53.0,
-            alignment: Alignment.center,
-            child: Text(
-              DateFormatter.getVerboseDate(dt.millisecondsSinceEpoch),
-              style: Theme.of(context)
-                  .textTheme
-                  .headline2!
-                  .copyWith(fontSize: 11, fontWeight: FontWeight.w500),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        );
-      },
-      indexedItemBuilder: (_, message, index) {
-        //conditions for determining the shape of the bubble sides
-        final List<bool> bubbleSides = bubbleSide(messages, index, true);
-        return SwipeActionCell(
-          controller: swipeController,
-          key: ObjectKey(messages[index]),
-          performsFirstActionWithFullSwipe: true,
-          fullSwipeFactor: 0.1,
-          trailingActions: <SwipeAction>[
-            SwipeAction(
-                content: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 25,
-                      height: 25,
-                      child: Image.asset(
-                        'assets/images/reply.png',
+    return SearchableChatView(
+        groupChatController: _controller,
+        reverse: true,
+        messages: messages,
+        indexedItemBuilder: (_, message, index) {
+          //conditions for determining the shape of the bubble sides
+          final List<bool> bubbleSides = bubbleSide(messages, index, true);
+          return SwipeActionCell(
+            controller: swipeController,
+            key: ObjectKey(messages[index]),
+            performsFirstActionWithFullSwipe: true,
+            fullSwipeFactor: 0.1,
+            trailingActions: <SwipeAction>[
+              SwipeAction(
+                  content: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 25,
+                        height: 25,
+                        child: Image.asset(
+                          'assets/images/reply.png',
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                  ],
-                ),
-                onTap: (CompletionHandler handler) async {
-                  Get.find<ThreadMessagesCubit>().reset();
+                      SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
+                  onTap: (CompletionHandler handler) async {
+                    Get.find<ThreadMessagesCubit>().reset();
 
-                  await NavigatorService.instance.navigate(
-                    channelId: message.channelId,
-                    threadId: message.id,
-                    reloadThreads: false,
-                  );
-                  swipeController.closeAllOpenCell();
-                },
-                color: Colors.transparent),
-          ],
-          child: (index == messages.length - 1 && endOfHistory)
-              ? ChannelFirstMessage(
-                  channel: widget.parentChannel, icon: message.picture ?? "")
-              : MessageTile<ChannelMessagesCubit>(
-                  message: message,
-                  upBubbleSide: bubbleSides[0],
-                  downBubbleSide: bubbleSides[1],
-                  key: ValueKey(message.hash),
-                  channel: widget.parentChannel,
-                ),
-        );
-      },
-    );
+                    await NavigatorService.instance.navigate(
+                      channelId: message.channelId,
+                      threadId: message.id,
+                      reloadThreads: false,
+                    );
+                    swipeController.closeAllOpenCell();
+                  },
+                  color: Colors.transparent),
+            ],
+            child: (index == messages.length - 1 && endOfHistory)
+                ? ChannelFirstMessage(
+                    channel: widget.parentChannel, icon: message.picture ?? "")
+                : MessageTile<ChannelMessagesCubit>(
+                    message: message,
+                    upBubbleSide: bubbleSides[0],
+                    downBubbleSide: bubbleSides[1],
+                    key: ValueKey(message.hash),
+                    channel: widget.parentChannel,
+                  ),
+          );
+        });
   }
 }
