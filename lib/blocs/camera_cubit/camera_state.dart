a45d9abed9eec003e11beaeb244828b98efb396e
EvGen94 <sharanov_evgenii@mail.ru>
2022-05-24 16:54:36 +0400

    camera setup

--- lib/blocs/camera_cubit/camera_state.dart
+++ lib/blocs/camera_cubit/camera_state.dart
@@ -4,21 +4,30 @@ enum CameraStateStatus { init, loading, found, done, failed }
 
 class CameraState extends Equatable {
   final CameraStateStatus cameraStateStatus;
-
   final List<CameraDescription> availableCameras;
-
-  const CameraState(
-      {this.cameraStateStatus = CameraStateStatus.init,
-      this.availableCameras = const []});
+  final int flashMode;
+  final bool cameraLensModeSwitch;
+  const CameraState({
+    this.cameraStateStatus = CameraStateStatus.init,
+    this.availableCameras = const [],
+    this.flashMode = 0,
+    this.cameraLensModeSwitch = false,
+  });
 
   CameraState copyWith(
       {CameraStateStatus? newCameraStateStatus,
-      List<CameraDescription>? newAvailableCameras}) {
+      List<CameraDescription>? newAvailableCameras,
+      int? newFlashMode,
+      bool? newCameraLensModeSwitch}) {
     return CameraState(
         cameraStateStatus: newCameraStateStatus ?? this.cameraStateStatus,
-        availableCameras: newAvailableCameras ?? this.availableCameras);
+        availableCameras: newAvailableCameras ?? this.availableCameras,
+        flashMode: newFlashMode ?? this.flashMode,
+        cameraLensModeSwitch:
+            newCameraLensModeSwitch ?? this.cameraLensModeSwitch);
   }
 
   @override
-  List<Object?> get props => [cameraStateStatus, availableCameras];
+  List<Object?> get props =>
+      [cameraStateStatus, availableCameras, flashMode, cameraLensModeSwitch];
 }
