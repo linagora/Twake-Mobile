def24a1bf666a6f9cce815fdf95346d3c7193641
sherlockvn <quangkhai28999@gmail.com>
2022-07-19 16:57:58 +0700

    when new message in thread arrive, mark channel as read

--- lib/blocs/messages_cubit/messages_cubit.dart
+++ lib/blocs/messages_cubit/messages_cubit.dart
@@ -297,8 +297,6 @@ abstract class BaseMessagesCubit extends Cubit<MessagesState> {
       //after message is send successfully mark channel as read
       _channelsCubit.markChannelRead(channelId: message.channelId);
     }
-    //after message is send successfully mark channel as read
-    
 
     _sendInProgress -= 1;
   }
@@ -592,10 +590,13 @@ class ChannelMessagesCubit extends BaseMessagesCubit {
 
             messages[i] = message;
 
-            final newState = MessagesLoadSuccess(
+            // mark message as read
+            _channelsCubit.markChannelRead(channelId: message.channelId);
+            final newState = MessageLatestSuccess(
               messages: messages,
               hash:
                   hash + message.hash, // just update the hash to force rerender
+              latestMessage: message,
             );
 
             emit(newState);
