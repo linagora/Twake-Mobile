1f25dbba2fc773d5c60513c86902c365ce5b0413
EvGen94 <sharanov_evgenii@mail.ru>
2022-08-29 18:58:10 +0300

    Quote message - Data

--- lib/sql/migrations.dart
+++ lib/sql/migrations.dart
@@ -1,6 +1,7 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:twake/sql/v11.dart';
 import 'package:twake/sql/v10.dart';
+import 'package:twake/sql/v13.dart';
 import 'package:twake/sql/v6.dart';
 import 'package:twake/sql/v8.dart';
 import 'package:twake/sql/v9.dart';
@@ -9,15 +10,16 @@ import 'package:twake/sql/v12.dart';
 // Base curent migration
 const CURRENT_MIGRATION = DDL_V6;
 // The latest versions
-const DBVER = DB_V12;
+const DBVER = DB_V13;
 // Add latest needed changes
-const List<int> DB_VERSIONS = [DB_V8, DB_V9, DB_V10, DB_V11, DB_V12];
+const List<int> DB_VERSIONS = [DB_V8, DB_V9, DB_V10, DB_V11, DB_V12, DB_V13];
 const List<List<String>> MIGRATIONS = [
   MIGRATION_8,
   MIGRATION_9,
   MIGRATION_10,
   MIGRATION_11,
   MIGRATION_12,
+  MIGRATION_13,
 ];
 
 Future<void> dbUpgrade(
